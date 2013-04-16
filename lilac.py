6241730cd62e5b6e2a70e53703b7a6d21d46e24e
Lilac <scatteredlilac@gmail.com>
2013-04-16 10:31:31 +0800

    Update lilac.py

--- lilac.py
+++ lilac.py
@@ -1,2 +1,43 @@
 #!/usr/bin/python
---*code:utf8-*-
+-*-coding:utf-8-*-
+
+import tornado.web
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+
+import os.path
+
+
+
+class LilacPicApp(tornado.web.Application): 
+  
+  def __init(self, **kargs):
+    
+    self.settings = { 
+                    static_path = os.path.join(os.path.dirname(__file__), "static"),
+                    template_path = os.path.join(os.path.dirname(__file__), "templates"),
+                    debug = True,
+                    cookie_secret = "bZJc2sWbQLKos6GkHn/VB9oXwQt8S0R0kRvJ5/xJ89E=",
+                    }
+    sefl.db_seetings = {
+      }
+                    
+    self.handlers = [(r'/', IndexRequest),
+                   (r'/about', AboutRequest),
+                   (r'/sitemap', SiteMapRequest),
+      
+                   ]
+    tornado.web.Application.__init__(self, self.handlers, self.settings)
+    
+  
+if __name__ == "__main__":
+    
+    tornado.options.parse_command_line()
+    
+    server = tornado.httpserver.HTTPServer(LilacPicApp())
+    
+    server.listen(options.port)
+    
+    tornado.ioloop.IOLoop.instance().start()
+    
